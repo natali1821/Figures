4fa573efb3204b97324da3afc0dea3a5199e61c7
natali1821 <ermolina_natali@bk.ru>
2021-03-15 18:32:35 +0300

    Add class Rectangle

---
+++ Rectangle.cpp
@@ -0,0 +1,5 @@
+#include "Rectangle.h"
+
+Rectangle::Rectangle(double a, double b) : Parallelogram(a, b, 90) {
+    _name = "Rectangle";
+}
---
+++ Rectangle.h
@@ -0,0 +1,10 @@
+#ifndef RECTANGLE_H_INCLUDED
+#define RECTANGLE_H_INCLUDED
+#include "Parallelogram.h"
+
+class Rectangle : public Parallelogram {
+public:
+    Rectangle(double a, double b);
+};
+
+#endif // RECTANGLE_H_INCLUDED
