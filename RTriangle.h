69d079a2598920e33310fee5298ea74e11ef2564
natali1821 <ermolina_natali@bk.ru>
2021-03-15 18:29:55 +0300

    Add class RTriangle

---
+++ RTriangle.h
@@ -0,0 +1,10 @@
+#ifndef RTRIANGLE_H_INCLUDED
+#define RTRIANGLE_H_INCLUDED
+#include "Triangle.h"
+
+class RTriangle : public Triangle {
+public:
+    RTriangle(double a, double b);
+};
+
+#endif // RTRIANGLE_H_INCLUDED
