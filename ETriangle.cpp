b9159122642a64220abde5ff183ce918d63a6b30
natali1821 <ermolina_natali@bk.ru>
2021-03-15 18:34:34 +0300

    Add class ETriangle

---
+++ ETriangle.cpp
@@ -0,0 +1,5 @@
+#include "ETriangle.h"
+
+ETriangle::ETriangle(double a) : ITriangle (a, a) {
+    _name = "ETriangle";
+}
---
+++ ETriangle.h
@@ -0,0 +1,10 @@
+#ifndef ETRIANGLE_H_INCLUDED
+#define ETRIANGLE_H_INCLUDED
+#include "ITriangle.h"
+
+class ETriangle : public ITriangle {
+public:
+    ETriangle(double a);
+};
+
+#endif // ETRIANGLE_H_INCLUDED
